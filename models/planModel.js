126e104e9887139f30f631f90c9add2046ba689b
Salvador Rodriguez <chava.crb@gmail.com>
2025-02-13 16:51:39 -0600

    Add training plan and workout models with validation

--- models/planModel.js
+++ models/planModel.js
@@ -1,5 +1,52 @@
 const mongoose = require('mongoose');
+const Workout = require('./workoutModel');
+
 
 const trainingPlanSchema = new mongoose.Schema({
-    
+    /* 
+    Based on a calendar week, a training plan will have a list of workouts for each day of the week.
+    The week will start on Monday and end on Sunday. And a last column will be for the total distance for the week.
+    The user can enter a comment for each workout. 
+    */
+    date: {
+        type: Date,
+        required: [true, 'Date is required'],
+    },
+    week: {
+        type: Number,
+        required: [true, 'Week is required'],
+    },
+    workouts: [
+        {
+            day: {
+                type: String,
+                enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                required: [true, 'Day is required'],
+            },
+            workout: {
+                type: mongoose.Schema.ObjectId,
+                ref: 'Workout',
+                required: [true, 'Workout is required'],
+            },
+            comment: [
+                {
+                    type: String,
+                    trim: true,
+                    maxlength: [250, 'Comment cannot exceed 200 characters'],
+                }
+            ],
+        },
+    ],
+    totalDistance: {
+        type: Number,
+        default: 0,
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'User is required'],
+    },
 });
+
+
+const TrainingPlan = mongoose.model('TrainingPlan', trainingPlanSchema);
---
+++ models/workoutModel.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+
+const splitSchema = new mongoose.Schema({
+  distance: {
+    type: Number,
+    unit: {
+      type: String,
+      enum: ["m", "km", "mi"],
+    },
+  },
+  time: String,
+  pace: String,
+});
+
+const paceSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["easy", "marathon", "tempo", "threshold", "interval", "repetition"],
+    required: [true, "Pace type is required"],
+  },
+  pace: {
+    type: String,
+    /* format match in mm:ss */
+    match: [/^([0-5][0-9]):[0-5][0-9]$/, "Please provide pace in mm:ss format"],
+    required: [true, "Pace is required"],
+  },
+});
+
+const workoutSchema = new mongoose.Schema({
+  workoutName: {
+    type: String,
+    required: [true, "Workout name is required"],
+    trim: true,
+    minlength: [2, "Workout name must be at least 2 characters"],
+    maxlength: [50, "Workout name cannot exceed 50 characters"],
+  },
+  warmUp: {
+    type: String,
+    pace: {
+      type: paceSchema,
+      default: null,
+    },
+    splits: [splitSchema],
+  },
+  work: [
+    {
+      type: {
+        type: String,
+        enum: ["distance", "time"],
+        required: [true, "Work type is required"],
+      },
+      // if type is distance then ask for a distance
+      distance: {
+        type: Number,
+        unit: {
+          type: String,
+          enum: ["m", "km", "mi"],
+        },
+      },
+      // if type is time then ask for a time
+      time: String,
+      pace: {
+        type: paceSchema,
+        default: null,
+      },
+      repetitions: {
+        type: Number,
+        default: 1,
+      },
+      splits: [splitSchema],
+    },
+  ],
+  coolDown: {
+    type: String,
+    pace: {
+      type: paceSchema,
+      default: null,
+    },
+    splits: [splitSchema],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "User is required"],
+  },
+}, {
+    timestamps: true,
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
