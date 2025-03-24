362ac7bf8f4a545fd15a3d3789b4694286d88312
Salvador Rodriguez <chava.crb@gmail.com>
2025-03-24 17:04:50 -0600

    Add user verification middleware and routes for user management

---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {updatePBs, updateUser, getAllUsers, getUser, deleteUser, setVdot} from "../controllers/userController.js";
+import { verifyToken } from "../middleware/verifyUser.js";
+
+const router = express.Router();
+
+// Update user
+router.put("/:id", verifyToken, updateUser);
+// Update personal bests
+router.put("/pb/:id", verifyToken, updatePBs);
+// Get user
+router.get("/:id", verifyToken, getUser);
+// Get all users
+router.get("/", verifyToken, getAllUsers);
+// Delete user
+router.delete("/:id", verifyToken, deleteUser);
+// Set Vdot
+router.put("/vdot/:id", verifyToken, setVdot);
+// Get all users
+router.get("/", verifyToken, getAllUsers);
+// Delete user
+router.delete("/:id", verifyToken, deleteUser);
